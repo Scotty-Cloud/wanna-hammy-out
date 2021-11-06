06515a1bcb013d0b629547274fef29a3813ca8d5
ScottR <astroplexity@gmail.com>
2021-11-06 17:06:06 -0400

    controllers import

---
+++ controllers/breeds.js
@@ -0,0 +1,9 @@
+import { Breed } from '../models/breed.js'
+
+function index(req, res) {
+  console.log("HAMPTER")
+}
+
+export {
+  index
+}
--- routes/breeds.js
+++ routes/breeds.js
@@ -1,6 +1,9 @@
 import { Router } from 'express'
+import * as breedsCtrl from '../controllers/breeds.js'
 
 const router = Router()
+
+router.get('/', breedsCtrl.index )
 
 export {
   router
