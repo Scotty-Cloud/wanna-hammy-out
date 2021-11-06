6b7307d1b5ab6164b476417677653285e4758e6f
ScottR <astroplexity@gmail.com>
2021-11-06 17:08:47 -0400

    ctrl function added

--- controllers/breeds.js
+++ controllers/breeds.js
@@ -1,7 +1,17 @@
 import { Breed } from '../models/breed.js'
 
 function index(req, res) {
-  console.log("HAMPTER")
+  Breed.find({})
+  .then(breeds => {
+    res.render('breeds/index', {
+      breeds,
+      title:"Hampsters!"
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect("/breeds")
+  })
 }
 
 export {
