0bbc7cbcd8361631a6aa8c6430d7f49301b1f104
ScottR <astroplexity@gmail.com>
2021-11-06 16:09:27 -0400

    breed model implemented

---
+++ models/breed.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const breedSchema = new Schema({
+  name: String,
+  aggressive: Boolean,
+  subSpecies: Boolean,
+  owner: {type: Schema.Types.ObjectId, 'ref': "profile"}
+})
+
+const Breed = mongoose.model('Breed', breedSchema)
+
+export {
+  Breed
+}
+
