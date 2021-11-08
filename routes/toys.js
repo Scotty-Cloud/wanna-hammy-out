b2ae8b0d2dd8cb5d83a5d6a7885cf9c2eb115fb3
ScottR <astroplexity@gmail.com>
2021-11-08 18:45:16 -0500

    update ctrl fixed

--- routes/toys.js
+++ routes/toys.js
@@ -4,23 +4,23 @@ import { isLoggedIn } from '../middleware/middleware.js'
 
 const router = Router()
 
-// localhost:3000/breeds - GET
+// localhost:3000/toys - GET
 router.get("/", toysCtrl.index )
-// localhost:3000/breeds/:id - GET
+// localhost:3000/toys/:id - GET
 router.get("/:id", toysCtrl.show)
-// localhost:3000/breeds/:id/edit
-router.get("/:id/edit", isLoggedIn, toysCtrl.edit)
+// localhost:3000/toys/:id/edit
+router.get("/:id/edit", toysCtrl.edit)
 
-// localhost:3000/breeds - POST
+// localhost:3000/toys - POST
 router.post("/", isLoggedIn, toysCtrl.create)
 
-// localhost:3000/breeds/:id/flip-aggressive - PATCH
-router.patch("/:id/flip-aggressive", isLoggedIn, toysCtrl.switchFun)
+// localhost:3000/toys/:id/flip-fun - PATCH
+router.patch("/:id/flip-fun", isLoggedIn, toysCtrl.switchFun)
 
-// localhost:3000/breeds/:id - PUT
+// localhost:3000/toys/:id - PUT
 router.put("/:id", isLoggedIn, toysCtrl.update)
 
-// localhost:3000/breeds/:id - DELETE
+// localhost:3000/toys/:id - DELETE
 router.delete("/:id", isLoggedIn, toysCtrl.delete)
 
 export {
