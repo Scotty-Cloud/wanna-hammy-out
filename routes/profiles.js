21b30bcbf911e695b1143dac403c41df13e20203
ScottR <astroplexity@gmail.com>
2021-11-08 19:29:22 -0500

    user profiles updated

--- routes/profiles.js
+++ routes/profiles.js
@@ -2,11 +2,11 @@ import { Router } from 'express'
 import { isLoggedIn } from '../middleware/middleware.js'
 import * as profilesCtrl from '../controllers/profiles.js'
 
-
 const router = Router()
 
 
 router.get("/", isLoggedIn, profilesCtrl.index)
+
 
 router.get("/:id", isLoggedIn, profilesCtrl.show)
 
